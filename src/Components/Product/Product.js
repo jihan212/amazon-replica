9b6948cb87d1373b7d54ecb25081bfa8854e0119
snapper <zihanjashim2121@gmail.comm>
2021-02-27 23:26:57 +0600

    Amazon replica

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
+    const {img,name,seller,price,stock} = props.product;
+    console.log(props.product);
     return (
-        <div>
-            <h1>This is Product</h1>
-            <h3>{props.product.name}</h3>
+        <div className = "product">
+            <div className = "product-img">
+            <img src={img} alt=""/>
+            </div>
+            <div>
+            <h4>{name}</h4>
+            <p><small>by:{seller}</small></p>
+            <br/>
+            <p>Price: $ {price}</p>
+            <br/>
+            <p> Only left {stock} in stock</p>
+            <br/>
+            <button className="main-btn"><FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
+            </div>
         </div>
     );
 };
